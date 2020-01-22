9fc69c1ceb46928bbd4c94aae76f1192d68aff17
Mateusz <mateuszd2411@gmail.com>
2020-01-22 10:56:05 +0100

    13.00   full1
    Signed-off-by: Mateusz <mateuszd2411@gmail.com>

--- app/src/main/java/com/example/socialnetwork/SetupActivity.java
+++ app/src/main/java/com/example/socialnetwork/SetupActivity.java
@@ -1,10 +1,24 @@
 package com.example.socialnetwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,6 +27,12 @@ public class SetupActivity extends AppCompatActivity {
     private EditText UserName, FullName, CountryName;
     private Button SaveInformationbutton;
     private CircleImageView ProfileImage;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
+
+    String currentUseID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +41,96 @@ public class SetupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUseID = mAuth.getCurrentUser().getUid();
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUseID);
+
         UserName = (EditText) findViewById(R.id.setup_username);
         FullName = (EditText) findViewById(R.id.setup_full_name);
         CountryName = (EditText) findViewById(R.id.setup_country_name);
         SaveInformationbutton = (Button) findViewById(R.id.setup_information_button);
         ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
+        loadingBar = new ProgressDialog(this);
 
+        SaveInformationbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SaveAccountSetupInformation();
+
+            }
+        });
+
+
+    }
+
+    private void SaveAccountSetupInformation() {
+
+        String username = UserName.getText().toString();
+        String fullname = FullName.getText().toString();
+        String country = CountryName.getText().toString();
+
+        if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(this, "Please write your username...", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(fullname))
+        {
+            Toast.makeText(this, "Please write your full name...", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(country))
+        {
+            Toast.makeText(this, "Please write your country...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Saving Information");
+            loadingBar.setMessage("Please wait we are creating your new account");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+
+            HashMap userMap = new HashMap();
+            userMap.put("username" , username);
+            userMap.put("fullname" , fullname);
+            userMap.put("country" , country);
+            userMap.put("status" , "Hey there");
+            userMap.put("gender" , "none");
+            userMap.put("dob" , "none");
+            userMap.put("relationshipstatus" , "none");
+            UserRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+
+                    if(task.isSuccessful())
+                    {
+                        SendUserToMainActivity();
+
+                        Toast.makeText(SetupActivity.this, "your Account is create Successfully.", Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+                    }
+                    else
+                    {
+                        String mesage = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Error Occurent: " + mesage, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+
+                }
+            });
+        }
+
+
+
+    }
+
+    private void SendUserToMainActivity() {
+
+        Intent mainIntent = new Intent(SetupActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
 
     }
 }
