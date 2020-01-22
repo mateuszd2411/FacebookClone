bbd665747e87740ef92dccbbded4a11503210370
Mateusz <mateuszd2411@gmail.com>
2020-01-22 17:45:35 +0100

    19.00   full1
    Signed-off-by: Mateusz <mateuszd2411@gmail.com>

--- app/src/main/java/com/example/socialnetwork/PostActivity.java
+++ app/src/main/java/com/example/socialnetwork/PostActivity.java
@@ -9,11 +9,20 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 
 public class PostActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+
+    private static final int Gallery_Pick = 1;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -22,6 +31,12 @@ public class PostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+
+        SelectPostImage = (ImageButton) findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button) findViewById(R.id.update_post_button);
+        PostDescription = (EditText) findViewById(R.id.post_descripion);
+
+
         mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -29,6 +44,24 @@ public class PostActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Update Post");
 
 
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                OpenGallery();
+
+            }
+        });
+
+
+    }
+
+    private void OpenGallery() {
+
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,Gallery_Pick );
     }
 
     @Override
