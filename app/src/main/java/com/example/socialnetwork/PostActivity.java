24ce335d608d242e682afb75e8283ae63371d4cb
Mateusz <mateuszd2411@gmail.com>
2020-01-22 17:21:34 +0100

    18.00   full1
    Signed-off-by: Mateusz <mateuszd2411@gmail.com>

--- app/src/main/java/com/example/socialnetwork/PostActivity.java
+++ app/src/main/java/com/example/socialnetwork/PostActivity.java
@@ -1,14 +1,55 @@
 package com.example.socialnetwork;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class PostActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            SendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    private void SendUserToMainActivity() {
+
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+
     }
 }
