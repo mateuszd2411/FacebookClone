7c91b0e37d003033d3dbdf096e2f72c8832fdda9
Mateusz <mateuszd2411@gmail.com>
2020-01-22 10:13:25 +0100

    11.00   full1
    Signed-off-by: Mateusz <mateuszd2411@gmail.com>

--- app/src/main/java/com/example/socialnetwork/MainActivity.java
+++ app/src/main/java/com/example/socialnetwork/MainActivity.java
@@ -129,7 +129,10 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.nav_Logout:
-                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+
+                mAuth.signOut();
+                SendUserToLoginActivity();
+
                 break;
         }
     }
