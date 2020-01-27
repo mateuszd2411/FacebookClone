073310ab18eef1f25206a08ebf235327d4ca6ffb
Mateusz <mateuszd2411@gmail.com>
2020-01-27 20:31:37 +0100

    47.00sw sw23
    Signed-off-by: Mateusz <mateuszd2411@gmail.com>

--- app/src/main/java/com/example/socialnetwork/PersonProfileActivity.java
+++ app/src/main/java/com/example/socialnetwork/PersonProfileActivity.java
@@ -1,14 +1,95 @@
 package com.example.socialnetwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PersonProfileActivity extends AppCompatActivity {
+
+    private TextView userName, userProfName, userStatus, userCountry, userGender,userRelation, userDOB;
+    private CircleImageView userProfileImage;
+    private Button SendFriendRequestButton, DeclineFrendRequestButton;
+
+    private DatabaseReference profileUserRef, UserRef;
+    private FirebaseAuth mAuth;
+    private String senderUserId, receivererUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_profile);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        receivererUserId = getIntent().getExtras().get("visit_user_id").toString();
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        IntializeFields();
+
+        UserRef.child(receivererUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists())
+                {
+                    String myProfileImage = dataSnapshot.child("profileimage").getValue().toString();
+                    String myUserName = dataSnapshot.child("username").getValue().toString();
+                    String myProfileName = dataSnapshot.child("fullname").getValue().toString();
+                    String myProfileStatus = dataSnapshot.child("status").getValue().toString();
+                    String myDOB = dataSnapshot.child("dob").getValue().toString();
+                    String myCountry = dataSnapshot.child("country").getValue().toString();
+                    String myGender = dataSnapshot.child("gender").getValue().toString();
+                    String myRelationStatus = dataSnapshot.child("relationshipstatus").getValue().toString();
+
+
+                    Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfileImage);
+
+                    userName.setText("@" + myUserName);
+                    userProfName.setText( myProfileName);
+                    userStatus.setText( myProfileStatus);
+                    userDOB.setText("DOB: " + myDOB);
+                    userCountry.setText("Country: " + myCountry);
+                    userGender.setText("Gender: " + myGender);
+                    userRelation.setText("Relationship: " + myRelationStatus);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+    private void IntializeFields() {
+
+        userName = (TextView) findViewById(R.id.person_username);
+        userProfName = (TextView) findViewById(R.id.person_full_name);
+        userStatus = (TextView) findViewById(R.id.person_profile_status);
+        userCountry = (TextView) findViewById(R.id.person_country);
+        userGender = (TextView) findViewById(R.id.person_gender);
+        userRelation = (TextView) findViewById(R.id.person_relationship_status);
+        userDOB = (TextView) findViewById(R.id.person_dob);
+        userProfileImage = (CircleImageView) findViewById(R.id.person_profile_pic);
+        SendFriendRequestButton = (Button)  findViewById(R.id.person_send_friend_request_btn);
+        DeclineFrendRequestButton = (Button)  findViewById(R.id.person_decline_friend_request);
     }
 }
